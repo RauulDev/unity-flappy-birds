8c4e90728aa932f9bbefa3737bfa6bd5e93f7d63
rmolina <rmolina@hexacta.com>
2023-02-28 19:33:20 -0500

    Added gameOverScreen

--- Assets/BirdScript2.cs
+++ Assets/BirdScript2.cs
@@ -6,16 +6,24 @@ public class BirdScript2 : MonoBehaviour
 {
     public Rigidbody2D MyRigidbody2D;
     public float flapStrength;
+    public LogicScript logic;
+    public bool birdIsAlive = true;
     // Start is called before the first frame update
     void Start()
     {
-
+        logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
             MyRigidbody2D.velocity = Vector2.up * flapStrength;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        logic.gameOver();
+        birdIsAlive = false;
     }
 }
--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,11 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
+using UnityEngine.SceneManagement;
 public class LogicScript : MonoBehaviour
 {
     public int playerScore;
     public Text scoreText;
+    public GameObject gameOverScreen;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,16 @@ public class LogicScript : MonoBehaviour
     {
         playerScore++;
         scoreText.text = playerScore.ToString();
+    }
+
+    public void restartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    public void gameOver()
+    {
+        gameOverScreen.SetActive(true);
     }
 
     // Update is called once per frame
